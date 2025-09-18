b7d3b63b4795b167efafe47135cae1206feb2fb4
Vitek_dolansky~ <vitek.dolansky.024@pslib.cz>
2025-09-18 13:03:16 +0200

    cotoje

--- moje-app/src/libs/sequences.ts
+++ moje-app/src/libs/sequences.ts
@@ -1,6 +1,14 @@
 export function generateArithmeticSequenceByCount(
     start: number, step: number, count: number
 ) {
-    
-return []
+    // osetreni vstupu
+if (Number.isFinite(start)) throw new Error ("pocatecni hodnota musi byt konecna")
+if (Number.isFinite(step)) throw new Error ("krok musi byt konecna hodnota")
+if (Number.isFinite(start) || count <= 0) throw new Error ("pocet prvku musi byt konecna hodnota vetsi nez 0")
+// vypocet
+const out: number[] = new Array();
+for(let i = 0; i < count; i++) {
+    out.push(start + i * step)
+}
+return out
 }
