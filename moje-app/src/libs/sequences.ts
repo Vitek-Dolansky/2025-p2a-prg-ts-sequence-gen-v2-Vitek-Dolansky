75a10721f3afa9813491be307de4957f12e4ab65
Vitek_dolansky~ <vitek.dolansky.024@pslib.cz>
2025-09-18 13:13:05 +0200

    cotojealefaktwtf

--- moje-app/src/libs/sequences.ts
+++ moje-app/src/libs/sequences.ts
@@ -1,14 +1,33 @@
 export function generateArithmeticSequenceByCount(
     start: number, step: number, count: number
-) {
-    // osetreni vstupu
-if (Number.isFinite(start)) throw new Error ("pocatecni hodnota musi byt konecna")
-if (Number.isFinite(step)) throw new Error ("krok musi byt konecna hodnota")
-if (Number.isFinite(start) || count <= 0) throw new Error ("pocet prvku musi byt konecna hodnota vetsi nez 0")
-// vypocet
-const out: number[] = new Array();
-for(let i = 0; i < count; i++) {
-    out.push(start + i * step)
+    )
+{   // ošetření vstupů
+    if (!Number.isFinite(start)) throw new Error("Počáteční hodnota musí být konečná");
+    if (!Number.isFinite(step)) throw new Error("Krok musí být konečná hodnota");
+    if ((!Number.isFinite(count)) || count <= 0) throw new Error("Počet prvků musí být konečná hodnota větší než nula");
+    // výpočet
+    const out: number[] = new Array();
+    for(let i = 0; i < count; i++) {
+        out.push(start + i * step);
+        // out[i] = start + i * step;
+    }
+    /*
+    for(let i = start; i <= start + i * step; i += step) {
+        out.push(i);
+    }
+    */
+    return out;
 }
-return out
+
+export function generateArithmeticSequenceByLastValue(
+    start: number,
+    step: number,
+    last: number
+    )
+{
+    // ošetření vstupů
+
+    // výpočet
+    const out: number[] = new Array();
+    return out;
 }
